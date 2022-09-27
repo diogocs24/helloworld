9eee06020121ffa492afe39bb8a2657a57f438c1
diogocs18 <diogopinguinhas@hotmail.com>
2022-09-28 00:49:14 +0100

    Added the version number

--- src/Application.java
+++ src/Application.java
@@ -1,1 +1,1 @@
-public class Application { public static void main(String args[]) { System.out.println("Hello World"); } }
+public class Application {  public static void main(String[] args) { System.out.println("Hello World");  System.out.println("Version 1.0");   } }
